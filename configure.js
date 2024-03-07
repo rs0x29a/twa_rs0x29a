14fbd4b24802a718245b7b7ef2699148d92ae0be
rs0x29a <adefka@gmail.com>
2024-03-07 14:33:55 +0300

    cfg

--- configure.js
+++ configure.js
@@ -70,7 +70,7 @@ let githubUsername, githubRepo, botUsername;
     {
       menu_button: {
         type: "web_app",
-        text: "Launch Webapp",
+        text: "rs0x29a",
         web_app: {
           url: url,
         },
